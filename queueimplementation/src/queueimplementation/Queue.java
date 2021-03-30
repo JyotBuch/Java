1ee26a9d9fd68fcbb1bc877aaa60d6af5240e658
JyotBuch <66951688+JyotBuch@users.noreply.github.com>
2021-03-30 19:07:43 +0530

    Add files via upload

---
+++ queueimplementation/src/queueimplementation/Queue.java
@@ -0,0 +1,89 @@
+package queueimplementation;
+
+import java.util.Scanner;
+
+public class Queue {
+	
+	private int arr[];
+	private int front;
+	private int rear;
+	private int capacity;
+
+	public Queue(int i) {
+		// TODO Auto-generated constructor stub
+		arr= new int[i];
+		front=-1;
+		rear=-1;
+		capacity=i;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue q = new Queue(5);
+		q.enQueue(1);
+		q.enQueue(2);
+		q.enQueue(3);
+		q.enQueue(4);
+		q.deQueue();
+		q.displayQueue();
+	}
+
+	private void displayQueue() {
+		// TODO Auto-generated method stub
+		for(int i=front;i<=rear;i++)
+			System.out.print(arr[i]+" ");
+	}
+
+	private int deQueue() {
+		// TODO Auto-generated method stub
+		int element;
+		if (isEmpty()) {
+		      System.out.println("Queue Empty");
+		      return -1;
+		      //System.exit(1);
+		    }
+		else{
+			element = arr[front];
+		      if (front >= rear) {
+		        front = -1;
+		        rear = -1;
+		      }
+		else {
+		    	front++;  
+		      }
+		      System.out.println("Removing item "+element);
+		      return(element);
+		}
+	}
+
+	private boolean isEmpty() {
+		// TODO Auto-generated method stub
+		if(front==-1)
+			return true;
+		else
+			return false;
+	}
+
+	private void enQueue(int i) {
+		// TODO Auto-generated method stub
+		if (isFull()) {
+		      System.out.println("Queue OverFlow");
+		      //System.exit(1);
+		    }
+		else if(front==-1)
+			front=0;
+		rear++;
+		arr[rear]=i;
+		System.out.println("Added item "+i);
+		
+	}
+
+	private boolean isFull() {
+		// TODO Auto-generated method stub
+		if (front == 0 && rear == capacity-1) {
+		      return true;
+		    }
+		    return false;
+		  }
+	}
+
