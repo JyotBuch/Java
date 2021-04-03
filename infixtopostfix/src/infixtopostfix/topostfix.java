9f7cf7cc82ab0231fd33fbcacf0eb23c2a1ddcff
JyotBuch <66951688+JyotBuch@users.noreply.github.com>
2021-04-03 17:20:28 +0530

    Add files via upload

---
+++ infixtopostfix/src/infixtopostfix/topostfix.java
@@ -0,0 +1,69 @@
+package infixtopostfix;
+
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Stack;
+ 
+class topostfix
+{
+    public static void ToPostfix(String infix)
+    {
+        Stack<Character> stack = new Stack<>();
+        for (int i=0;i<infix.length();i++)
+        {
+			char c=infix.charAt(i);
+            if (c == '(') {
+                stack.add(c);
+            }
+            else if (c == ')')
+            {
+                while (stack.peek() != '(') {
+					System.out.print(stack.pop());
+                }
+                stack.pop();
+            }
+            else if (isOperand(c)) {
+				System.out.print(c);
+            }
+            else {
+                while (!stack.isEmpty() && prec(c) >= prec(stack.peek())) {
+					System.out.print(stack.pop());
+                }
+                stack.add(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+			System.out.print(stack.pop());
+        }
+    }
+	public static int prec(char c)
+    {
+        if (c == '×' || c == '/' || c=='*') {
+            return 1;
+        }
+ 
+        if (c == '+' || c == '-') {
+            return 2;
+        }
+        if (c == '^') {
+            return 3;
+        }
+        if (c == '|') {
+            return 4;
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    public static boolean isOperand(char c)
+    {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
+            (c >= '0' && c <= '9');
+    }
+ 
+    public static void main(String[] args)
+    {
+        String infix = "a*b+c";
+ 
+        ToPostfix(infix);
+    }
+}
