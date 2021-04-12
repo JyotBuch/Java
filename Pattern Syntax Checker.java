8938c9ffd527a7b2a247f183b4363827b281882e
JyotBuch <66951688+JyotBuch@users.noreply.github.com>
2021-04-12 17:38:20 +0530

    Create Pattern Syntax Checker.java

---
+++ Pattern Syntax Checker.java
@@ -0,0 +1,28 @@
+//Solution to https://www.hackerrank.com/challenges/pattern-syntax-checker/problem
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		//int testCases = Integer.parseInt(in.nextLine());
+        for(int i=0;i<testCases;i++)
+        {
+            try
+            {
+                String pattern = in.nextLine();
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            }
+
+            catch (Exception e)
+            {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
+
