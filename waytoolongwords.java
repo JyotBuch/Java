6495a1f06cd71c180e9b7dc75909e87c50014173
JyotBuch <66951688+JyotBuch@users.noreply.github.com>
2021-04-19 14:18:27 +0530

    Add files via upload

---
+++ waytoolongwords.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class waytoolongwords {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        String words[]=new String[n];
+        for(int i=0;i<n;i++)
+        {
+            words[i]=sc.next();
+            if(words[i].length()>10)
+            {
+                words[i]=getNewWord(words[i]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(words[i]);
+        }
+    }
+
+    public static String getNewWord(String word)
+    {
+        String count=Integer.toString(word.length()-2);
+        word=word.charAt(0)+count+word.charAt(word.length()-1);
+        return word;
+    }
+}
