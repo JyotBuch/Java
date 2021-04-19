90d2ad631ddaf2c368bcebc348279c70bfb5b1ca
JyotBuch <66951688+JyotBuch@users.noreply.github.com>
2021-04-19 14:32:48 +0530

    Create nextround.java

---
+++ nextround.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class nextround {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int score[]=new int[n];
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            score[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(score[i]>=score[k-1] && score[i]>0)
+            {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+        
+}
