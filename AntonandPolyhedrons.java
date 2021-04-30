eb66cbeb6c5452481ed217ba6cea40647f2c842e
JyotBuch <66951688+JyotBuch@users.noreply.github.com>
2021-04-30 20:33:49 +0530

    Add files via upload

---
+++ AntonandPolyhedrons.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AntonandPolyhedrons{
+    
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            if (s.equals("Tetrahedron")) res += 4;
+            else if (s.equals("Cube")) res += 6;
+            else if (s.equals("Octahedron")) res += 8;
+            else if (s.equals("Dodecahedron")) res += 12;
+            else if (s.equals("Icosahedron")) res += 20;
+        }
+        System.out.println(res);
+    }
+}
